68f8e4268505cfdbc278eb4d2250c04a92be8fbc
utsav_brahmbhatt <utsavbrahmbhatt23@gmail.com>
2021-08-05 19:48:37 +0530

    Implemented Multiply func() from local repo

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtract(x,y):
     return x-y      #from remote
 # Multiply Implementation
 def multiply(x,y):
-    pass
+    return x*y   #from local repo
 # Divide Implementation
 def divide(x,y):
     pass
